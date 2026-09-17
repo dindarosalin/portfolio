d9bd07b7d7a75061f69fa2f62e990708f6d3eeae
Dinda Rosalin <dindarosalin24@gmail.com>
2026-09-17 19:01:00 +0700

    merapikan struktur direktory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 
-import Navbar from './components/navbar'
+import Navbar from './pages/public/navbar'
 import Home from './pages/public/home'
 import About from './pages/public/about'
-// import Learn from './pages/public/learn'
 import Projects from './pages/public/projects'
 import Experiences from './pages/public/experiences'
 import Certifications from './pages/public/certifications'
@@ -12,19 +11,20 @@ import Footer from './pages/public/footer'
 
 function App() {
   return (
-    <main>
+    <>
       <Navbar />
 
-      <Home />
-      <About />
-      {/* <Learn /> */}
-      <Projects />
-      <Experiences />
-      <Certifications />
-      <Contact />
+      <main>
+        <Home />
+        <About />
+        <Projects />
+        <Experiences />
+        <Certifications />
+        <Contact />
+      </main>
 
       <Footer />
-    </main>
+    </>
   )
 }
 
